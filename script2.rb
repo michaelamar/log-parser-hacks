0ddc40fe02038a3ad716a15ddd09ee6aa71a55b5
Michael Amar <Mordachiamar@gmail.com>
2014-01-24 14:38:36 -0500

    fixed up the last results of the array so that the theirs a minimum of 3 in each array

--- script2.rb
+++ script2.rb
@@ -1,18 +1,31 @@
 range=0
 
 a=Hash.new
-a={1 => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k"]}
+a={1 => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k"] }
 b=Array.new
 c=Array.new
 d=Array.new(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k"])
 
-d.each do |x|
-	for i in range..range+2
-		c=d[i].to_a
-		b<<c
+# d.each do |x|
+# 	for i in range..range+2
+# 		c=d[i].to_a
+# 		b<<c
+# 	end
+# 	range=range+1
+# end
+
+
+for i in (1..(a.length))
+	s=a[i]
+	s.each do |x|
+		if s.index(x)<s.length-2
+			for i in range..range+2
+ 				c=d[i].to_s
+				b<<c
+		 		end
+	 		range=range+1
+		end
 	end
-	range=range+1
 end
-
-
 puts b
+
